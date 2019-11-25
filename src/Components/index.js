3f3000a7154be329f76a4bc45e9c721a346b8d16
Alberto <albertopumar96@gmail.com>
2019-11-25 20:05:48 +0100

    Export all components to allow override

---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+import TableHeader from './TableHeader';
+import StyledHeaderWrapper from './TableHeader/StyledHeaderWrapper';
+
+import TableCell from './TableHeader/HeaderCell';
+import Filter from './TableHeader/Filter';
+
+export { TableHeader, StyledHeaderWrapper, TableCell, Filter };
