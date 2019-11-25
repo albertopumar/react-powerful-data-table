32de4d58d6fd44871f86e8cb28b54e9065eda7f4
Alberto <albertopumar96@gmail.com>
2019-11-25 17:17:50 +0100

    Debounce filter input change

--- src/Components/TableHeader/Filter.js
+++ src/Components/TableHeader/Filter.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import _ from 'lodash';
+
+import { DEBOUNCE_TIME } from '../../constants';
 
 const SearchInput = styled.input`
     background: none;
@@ -17,14 +20,15 @@ const ClearIcon = styled.span`
 const FilterInput = props => {
     const searchInputRef = React.createRef();
 
-    const handleClearClick = () => {
-        searchInputRef.current.value = '';
-        _handleFilterChange('');
-    };
-
     const _handleFilterChange = data => {
         const { columnStructure, handleFilterChange } = props;
         handleFilterChange({ data, field: columnStructure.field });
+    };
+    const debounceFilter = _.debounce(_handleFilterChange, DEBOUNCE_TIME);
+
+    const handleClearClick = () => {
+        searchInputRef.current.value = '';
+        _handleFilterChange('');
     };
 
     return (
@@ -32,7 +36,7 @@ const FilterInput = props => {
             <SearchInput
                 type="text"
                 placeholder="Search"
-                onChange={({ target: { value } }) => handleFilterChange(value)}
+                onChange={({ target: { value } }) => debounceFilter(value)}
                 ref={searchInputRef}
             />
             <ClearIcon onClick={handleClearClick}>X</ClearIcon>
