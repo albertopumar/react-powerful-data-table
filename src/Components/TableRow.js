e9f2b44c7b3bd97b30e1b56000a5b0f76159cccc
Alberto <albertopumar96@gmail.com>
2019-11-25 20:04:52 +0100

    Utils import

--- src/Components/TableRow.js
+++ src/Components/TableRow.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 
-import ComponentsContext from './Utils/ComponentsContext';
+import ComponentsContext from '../Utils/ComponentsContext';
 
 const StyledRow = styled.div`
     display: flex;
