1d0d201e83a55e13c24db805d4ae23ceb422ff93
Alberto <albertopumar96@gmail.com>
2019-12-09 14:39:55 +0100

    Pagination rename

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -24,19 +24,19 @@ const StyledPageButton = styled.div`
     color: ${props => (props.selected ? '#3fc5f0' : 'rgba(0, 0, 0, 0.65)')};
 `;
 
-const Pagination = ({ paging, handlePageChange }) => {
+const Pagination = ({ pagination, handlePageChange }) => {
     //TODO: Responsive mobile pagination
     //TODO: Change next and prev icons
 
-    const pageNumber = Math.ceil(paging.totalCount / paging.pageSize);
+    const pageNumber = Math.ceil(pagination.totalCount / pagination.pageSize);
 
-    const isPageStarting = paging.currentPage > 3;
-    const isPageEnding = paging.currentPage < pageNumber - 4;
+    const isPageStarting = pagination.currentPage > 3;
+    const isPageEnding = pagination.currentPage < pageNumber - 4;
 
     //TODO: refactor for legibility
     const pages = [...Array(pageNumber).keys()].slice(
-        isPageStarting ? (!isPageEnding && pageNumber > 7 ? pageNumber - 5 : paging.currentPage - 1) : 0,
-        isPageEnding ? (!isPageStarting && pageNumber > 7 ? 5 : paging.currentPage + 2) : pageNumber
+        isPageStarting ? (!isPageEnding && pageNumber > 7 ? pageNumber - 5 : pagination.currentPage - 1) : 0,
+        isPageEnding ? (!isPageStarting && pageNumber > 7 ? 5 : pagination.currentPage + 2) : pageNumber
     );
 
     const context = useContext(ComponentsContext);
@@ -48,13 +48,13 @@ const Pagination = ({ paging, handlePageChange }) => {
     return (
         <PaginationWrapper>
             <PageButton
-                disabled={paging.currentPage === 0}
-                onClick={() => paging.currentPage > 0 && handlePageChange(paging.currentPage - 1)}
+                disabled={pagination.currentPage === 0}
+                onClick={() => pagination.currentPage > 0 && handlePageChange(pagination.currentPage - 1)}
             >
                 {'<'}
             </PageButton>
 
-            {paging.currentPage > 3 && (
+            {pagination.currentPage > 3 && (
                 <React.Fragment>
                     <PageButton onClick={() => handlePageChange(0)}>1</PageButton>
                     <PageButton>...</PageButton>
@@ -64,14 +64,14 @@ const Pagination = ({ paging, handlePageChange }) => {
             {pages.map(page => (
                 <PageButton
                     key={`page-key-${page}`}
-                    selected={paging.currentPage === page}
+                    selected={pagination.currentPage === page}
                     onClick={() => handlePageChange(page)}
                 >
                     {page + 1}
                 </PageButton>
             ))}
 
-            {paging.currentPage < pageNumber - 4 && (
+            {pagination.currentPage < pageNumber - 4 && (
                 <React.Fragment>
                     <PageButton>...</PageButton>
                     <PageButton onClick={() => handlePageChange(pageNumber - 1)}>{pageNumber}</PageButton>
@@ -79,8 +79,8 @@ const Pagination = ({ paging, handlePageChange }) => {
             )}
 
             <PageButton
-                disabled={paging.currentPage === pageNumber - 1}
-                onClick={() => paging.currentPage < pageNumber - 1 && handlePageChange(paging.currentPage + 1)}
+                disabled={pagination.currentPage === pageNumber - 1}
+                onClick={() => pagination.currentPage < pageNumber - 1 && handlePageChange(pagination.currentPage + 1)}
             >
                 {'>'}
             </PageButton>
