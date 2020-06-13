5a6de66b23058e5db5eff025e1e3ff9884490513
Alberto <albertopumar96@gmail.com>
2020-06-13 12:51:47 +0200

    Restructure weapper component

--- src/Components/TableStructure.js
+++ src/Components/TableStructure.js
@@ -1,18 +1,11 @@
 import React, { useContext } from 'react';
-import styled from 'styled-components';
 
 import TableHeader from './TableHeader';
 import ComponentsContext from '../Utils/ComponentsContext';
 import TableContent from './TableContent';
 import DefaultPagination from './Pagination';
 
-// TODO: extract to component
-const TableWrapper = styled.div`
-    width: 80%;
-    max-width: 900px;
-    margin: auto;
-    font-family: Verdana;
-`;
+import TableWrapper from './TableWrapper.styled';
 
 export default props => {
     const {
---
+++ src/Components/TableWrapper.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const TableWrapper = styled.div`
+    width: 80%;
+    max-width: 900px;
+    margin: auto;
+    font-family: Verdana;
+`;
+
+export default TableWrapper;
